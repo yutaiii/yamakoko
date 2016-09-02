05749a2085ba4be9946059b5b57083882f4516a5
YutaIchikawa <d_m_b0704@yahoo.co.jp>
2016-09-03 06:49:43 +0900

    first commit no.2

---
+++ yamakoko_loginTop.php
@@ -0,0 +1,14 @@
+<?php
+	include('parts/yamakoko_head.php');
+	include('parts/yamakoko_header.php');
+?>
+
+	<div class="loginTop_main">
+	<div class="loginTop_backFilter">
+	</div>
+	<p>hiiiiiiii</p>
+	</div>
+
+<?php
+	include('parts/yamakoko_footer.php');
+?>
